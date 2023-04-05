62b46eebd7ee01d45cffd552e7f02ae66e64c41d
David Haoming Chen <63654528+20dchen01@users.noreply.github.com>
2023-04-05 15:52:46 +0100

    automatic rather than manual

--- main.py
+++ main.py
@@ -1,40 +1,68 @@
 import os
-import discord
 import requests
+import discord
+from discord.ext import tasks
 
 client = discord.Client()
 
+# Replace with your own Riot Games API key
+RIOT_API_KEY = os.environ['RIOT_API_KEY']
+# Replace with your own Discord API key
+DISCORD_API_KEY = os.environ['DISCORD_API_KEY']
+
+# The summoner name and region to track
+summoner_name = "Wraithlander"
+region = "euw1"
+
+# The ID of the channel to post updates in
+channel_id = 1234567890
+
+@tasks.loop(minutes=1)
+async def track_ranked_games():
+    try:
+        # Get summoner data
+        summoner_url = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}"
+        summoner_data = requests.get(summoner_url, headers={"X-Riot-Token": RIOT_API_KEY}).json()
+        summoner_id = summoner_data['id']
+
+        # Get match history
+        match_history_url = f"https://{region}.api.riotgames.com/lol/match/v4/matchlists/by-account/{summoner_id}?queue=420"
+        match_history_data = requests.get(match_history_url, headers={"X-Riot-Token": RIOT_API_KEY}).json()
+        match_id = match_history_data['matches'][0]['gameId']
+
+        # Get match data
+        match_url = f"https://{region}.api.riotgames.com/lol/match/v4/matches/{match_id}"
+        match_data = requests.get(match_url, headers={"X-Riot-Token": RIOT_API_KEY}).json()
+        participant_id = -1
+        for i in range(len(match_data['participantIdentities'])):
+            if match_data['participantIdentities'][i]['player']['summonerName'] == summoner_name:
+                participant_id = match_data['participantIdentities'][i]['participantId']
+                break
+        stats = match_data['participants'][participant_id - 1]['stats']
+        result = "Victory" if stats['win'] else "Defeat"
+        lp_gain = stats['win'] * (int(match_data['gameDuration']) // 60 + 3)
+
+        # Get champion data
+        champion_url = f"https://{region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summoner_id}"
+        champion_data = requests.get(champion_url, headers={"X-Riot-Token": RIOT_API_KEY}).json()
+        champion_id = match_data['participants'][participant_id - 1]['championId']
+        champion_name = ""
+        for champion in champion_data:
+            if champion['championId'] == champion_id:
+                champion_name = champion['championName']
+                break
+
+        # Post update in Discord channel
+        channel = client.get_channel(channel_id)
+        message = f"{summoner_name}'s last ranked game:\nResult: {result}\nLP gain: {lp_gain}\nChampion played: {champion_name}"
+        await channel.send(message)
+
+    except Exception as e:
+        print(f"Error tracking ranked games: {e}")
+
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+    print(f"Logged in as {client.user}")
+    track_ranked_games.start()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!track'):
-        # Retrieve Wraithlander's summoner ID using Riot Games API
-        response = requests.get('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Wraithlander',
-                                headers={'X-Riot-Token': os.environ['RIOT_API_KEY']})
-        summoner_id = response.json()['id']
-
-        # Retrieve Wraithlander's match history using Riot Games API
-        response = requests.get(f'https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/{summoner_id}?queue=420',
-                                headers={'X-Riot-Token': os.environ['RIOT_API_KEY']})
-        match_history = response.json()['matches']
-
-        # Retrieve information about the last match
-        response = requests.get(f'https://euw1.api.riotgames.com/lol/match/v4/matches/{match_history[0]["gameId"]}',
-                                headers={'X-Riot-Token': os.environ['RIOT_API_KEY']})
-        match_info = response.json()
-
-        # Parse information about the last match
-        champion_played = match_info['participantIdentities'][0]['player']['summonerName']
-        result = 'Victory' if match_info['participants'][0]['stats']['win'] else 'Defeat'
-        lp_gain = match_info['participants'][0]['stats']['leaguePoints']
-
-        # Send a message to the channel with information about the last match
-        await message.channel.send(f'Wraithlander just played {champion_played} and got {result} with {lp_gain} LP gain!')
-
-client.run(os.environ['DISCORD_API_TOKEN'])
+client.run(DISCORD_API_KEY)
